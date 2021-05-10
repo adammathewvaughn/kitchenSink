c09ca8b2f5c5b06be17d51bba7206c83c09ecad3
Adam Vaughn <adammathewvaughn>
2021-05-10 17:36:18 -0500

    tryna change the name

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const States = 50;//This declares, initializes and assigns value of '50' to the 
 let x = 5 + 4; //This declares, initializes, and assigns the computed value of '5 + 4' to 'x.' 
 
 function lionelRitchie() {
-  window.alert("Hello.... Is it me you're looking for? I can see it in your eyes, I can see it in your smile. You're all I've ever wanted and my arms are open wide.'Cause you know just what to say, and you know just what to do... And I want to tell you so much....I love you");
+  window.alert("Hello.... Is it me you're looking for? I can see it in your eyes, I can see it in your smile...");
 }
 
 lionelRitchie();
