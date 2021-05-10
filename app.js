f37dc7b0b10aca4f78127cfd2031dce199d1900f
Adam <adammathewvaughn@gmail.com>
2021-05-10 17:41:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+let name ="Adam";//This declares, initializes and assigns value of "Adam" to the "name" variable. 
+const States = 50;//This declares, initializes and assigns value of '50' to the constant "States"
+let x = 5 + 4; //This declares, initializes, and assigns the computed value of '5 + 4' to 'x.' 
 
+function lionelRitchie() {
+  window.alert("Hello.... Is it me you're looking for? I can see it in your eyes, I can see it in your smile. You're all I've ever wanted and my arms are open wide.'Cause you know just what to say, and you know just what to do... And I want to tell you so much....I love you");
+}
+
+lionelRitchie();
