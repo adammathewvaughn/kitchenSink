5635f1886bd28436b60acc0c062b8855d61a4f18
Adam Vaughn <adammathewvaughn>
2021-05-10 17:43:00 -0500

    Did I do it?

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+<<<<<<< HEAD
 let name ="Adam";//This declares, initializes and assigns value of "Adam" to the "name" variable. 
 const States = 50;//This declares, initializes and assigns value of '50' to the constant "States"
 let x = 5 + 4; //This declares, initializes, and assigns the computed value of '5 + 4' to 'x.' 
 
 function lionelRitchie() {
-  window.alert("Hello.... Is it me you're looking for? I can see it in your eyes, I can see it in your smile...");
+  window.alert("Hello.... Is it me you're looking for? I can see it in your eyes, I can see it in your smile. You're all I've ever wanted and my arms are open wide.'Cause you know just what to say, and you know just what to do... And I want to tell you so much....I love you");
 }
 
 lionelRitchie();
 
+=======
+let name ="Adam";//This declares, initializes and assigns value of "Adam" to the "name" variable. 
+const States = 50;//This declares, initializes and assigns value of '50' to the constant "States"
+let x = 5 + 4; //This declares, initializes, and assigns the computed value of '5 + 4' to 'x.' 
+
+function lionelRitchie() {
+  window.alert("Hello.... Is it me you're looking for? I can see it in your eyes, I can see it in your smile. You're all I've ever wanted and my arms are open wide.'Cause you know just what to say, and you know just what to do... And I want to tell you so much....I love you");
+}
+
+lionelRitchie();
+>>>>>>> f37dc7b0b10aca4f78127cfd2031dce199d1900f
